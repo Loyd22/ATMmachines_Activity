76ba1d273bbf78160c88eeb130d387f2f4f76e9c
Admin <Admin@YEL>
2024-04-12 14:51:20 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using atmlibrary;
+namespace ATMmachines
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ATMsmachines.Main(args);
+        }
+    }
+}
